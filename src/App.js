bdf7215593af233842b57bdb9d7c9c1ed41fb294
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-09-03 08:49:31 -0400

    state updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Data from "./Data.json";
-
+import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [currentPage,setCurrentPage]=useState(1);
+   const recordsPerPage=5;
+  const lastIndex=currentPage*recordsPerPage;
+  const firstIndex=lastIndex-recordsPerPage;
+  const records=Data.slice(firstIndex,lastIndex);
+  const npage=Math.ceil(Data.length/recordsPerPage);
+  const numbers=[...Array(npage+1).keys()].slice(1)
   return (
     <div>
       <table className="table">
